03e79284932850e3f8fc27d187bb008aae442e90
Ali Salam <asalam@MacBook-Pro-de-Ali.local>
2022-09-12 17:08:29 +0200

    50min

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { applyMiddleware, compose } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { reducers } from './reducers';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+//const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(<App />, document.getElementById('root'));
