f70964e8f8ebece23178f7edf778fd0a9fbde84e
Ali Salam <asalam@MacBook-Pro-de-Ali.local>
2022-09-16 15:31:38 +0200

    2h32

--- server/index.js
+++ server/index.js
@@ -2,22 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
-app.use(bodyParser.json({limit:"30mb", extended:"true"}));
-app.use(bodyParser.urlencoded({limit:"30mb", extended:"true"}));
+app.use(bodyParser.json({ limit: '30mb', extended: 'true' }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: 'true' }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://galactali:galactali123@cluster0.6agigvo.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+mongoose
+	.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+	.catch((error) => console.log(`${error} did not connect`));
 
 // mongoose.set('useFindAndModify', false);
