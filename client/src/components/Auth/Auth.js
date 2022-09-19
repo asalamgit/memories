dc28ab34138b994d905e183bfe17efe67d9d3e4d
Ali Salam <asalam@MacBook-Pro-de-Ali.local>
2022-09-19 11:21:07 +0200

    3h11

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -7,11 +7,19 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 const Auth = () => {
 	const classes = useStyles();
-	const [isSignup, setIsSignup] = useState(false);
+	const [isSignup, setIsSignup] = useState(true);
+	const [showPassword, setShowPassword] = useState(false);
+
+	const handleShowPassword = () => setShowPassword(!showPassword);
 
 	const handleSubmit = (e) => {};
 
 	const handleChange = (e) => {};
+
+	const switchMode = () => {
+		setIsSignup((prevIsSignup) => !prevIsSignup);
+		setShowPassword(false);
+	};
 
 	return (
 		<Container component="main" maxWidth="xs">
@@ -29,6 +37,16 @@ const Auth = () => {
 							</>
 						)}
 						<Input name="email" label="Email Adress" handleChange={handleChange} type="email" />
+						<Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+						{isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
+					</Grid>
+					<Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+						{isSignup ? 'Sign up' : 'Sign in'}
+					</Button>
+					<Grid container justifyContent="flex-end">
+						<Grid item>
+							<Button onClick={switchMode}>{isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign Up"}</Button>
+						</Grid>
 					</Grid>
 				</form>
 			</Paper>
