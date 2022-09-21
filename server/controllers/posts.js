f0febbe50f53852169f2e9018eaa5e04f43f330f
Ali Salam <asalam@MacBook-Pro-de-Ali.local>
2022-09-21 13:53:13 +0200

    4h20

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -44,17 +44,39 @@ export const deletePost = async (req, res) => {
 
 	await PostMessage.findByIdAndRemove(id);
 
-	res.json({ message: "Post deleted successfully." });
-}
+	res.json({ message: 'Post deleted successfully.' });
+};
+
+// export const likePost = async (req, res) => {
+// 	const { id } = req.params;
+
+// 	if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with that id `);
+
+// 	const post = await PostMessage.findById(id);
+
+// 	const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+// 	res.json(updatedPost);
+// }
 
 export const likePost = async (req, res) => {
 	const { id } = req.params;
 
-	if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with that id `);
-	
+	if (!req.userId) {
+		return res.json({ message: 'Unauthenticated' });
+	}
+
+	if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
 	const post = await PostMessage.findById(id);
 
-	const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
-	
-	res.json(updatedPost);
-}
+	const index = post.likes.findIndex((id) => id === String(req.userId));
+
+	if (index === -1) {
+		post.likes.push(req.userId);
+	} else {
+		post.likes = post.likes.filter((id) => id !== String(req.userId));
+	}
+	const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+	res.status(200).json(updatedPost);
+};
