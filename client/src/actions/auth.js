c6680e71cf39b19fc17553073d464f76b3765172
Ali Salam <asalam@MacBook-Pro-de-Ali.local>
2022-09-22 11:46:05 +0200

    4h31

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,9 +2,9 @@ import * as api from '../api/index.js';
 
 export const signin = (formData, router) => async (dispatch) => {
   try {
-    // const { data } = await api.signIn(formData);
+    const { data } = await api.signIn(formData);
 
-    // dispatch({ type: 'AUTH', data });
+    dispatch({ type: 'AUTH', data });
 
     router.push('/');
   } catch (error) {
@@ -14,9 +14,9 @@ export const signin = (formData, router) => async (dispatch) => {
 
 export const signup = (formData, router) => async (dispatch) => {
   try {
-    // const { data } = await api.signUp(formData);
+    const { data } = await api.signUp(formData);
 
-    // dispatch({ type: 'AUTH', data });
+    dispatch({ type: 'AUTH', data });
 
     router.push('/');
   } catch (error) {
