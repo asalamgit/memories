23ad4b0201951a1ad22b7541b4cb7ca2b579de71
Ali Salam <asalam@MacBook-Pro-de-Ali.local>
2022-09-28 12:24:47 +0200

    search back ok

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -13,7 +13,7 @@ export const getPosts = () => async (dispatch) => {
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
 	try {
 		// dispatch({ type: 'START_LOADING' });
-		const { data } = await api.fetchPostsBySearch(searchQuery);
+		const { data : {data}} = await api.fetchPostsBySearch(searchQuery);
 		console.log(data);
 		// dispatch({ type: 'FETCH_BY_SEARCH', payload: { data } });
 		// dispatch({ type: 'END_LOADING' });
